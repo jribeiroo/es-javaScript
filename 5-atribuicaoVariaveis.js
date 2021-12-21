8902f5b85cc51a562a569e78f6e9b18f89c82078
jribeiroo <nicolas.jnr10@outlook.com>
2021-12-21 16:09:57 -0300

    Atribuição e imutabilidade

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,17 @@
+console.log("Atribuição a variaveis");
+
+const idade = 29; //number const (constante não muda teu valor)
+let nome = "jorge"; // string let pode mudar (variação)
+
+const sobrenome = "ribeiro";
+
+console.log(`meu nome é ${nome} ${sobrenome}`)
+
+// ou 
+ nome = nome + sobrenome;
+
+ console.log(nome);
+
+ const nomeCompleto = nome + sobrenome;
+
+ console.log(nomeCompleto);
